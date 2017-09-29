6d4edcfc1cb52646846027520710fb6201129cb9
Xiaofeng Qu <xiaofeng.qu.83@gmail.com>
2017-09-28 22:35:07 -0400

    Separate the Class definition and Implementation

--- List.hpp
+++ List.hpp
@@ -2,6 +2,16 @@
 #define LIST_HPP_INCLUDED
 #include <iostream>
 #include "Constants.hpp"
+/*
+   COPYRIGHT (C) 2017 Xiaofeng Qu (xq6 ) All rights reserved.
+   List Template
+   Author.  Xiaofeng Qu
+            xq6@zips.uakron.edu
+   Version. 2.0 09.18.2017
+
+*/
+/* List template */
+/* Class definition */
 template <class T>
 class List{
 private:
@@ -28,80 +38,5 @@ public:
     int length() const;
     int find(T);
 };
-template <class T>
-List<T>::List(const List &l){
-    item = new T[MAX_EXPRESSION_LENGTH];
-    itemNumber = l.length() - 1;
-    for(int i=0; i<l.length(); i++){
-        item[i] = l.item[i];
-    }
-}
-
-template <class T>
-void List<T>::append(T it){
-    itemNumber++;
-    item[itemNumber] = it;
-}
-
-template <class T>
-T& List<T>::operator[](int n){
-    if(n >= length()) throw "Index out of bound.";
-    return item[n];
-}
-
-//template <class T>
-//void List<T>::setItemAtN(T it, int n){
-//    item[n] = it;
-//}
-
-//template <class T>
-//T List<T>::itemAtN(int n){
-//    return item[n];
-//}
-
-template <class T>
-void List<T>::printList(){
-    for(int i=0; i<length(); i++){
-        std::cout << item[i] << " ";
-    }
-}
-
-template <class T>
-void List<T>::delNode(T it){
-    int index = find(it);
-    if(index==-1) return;
-    for(int i=index; i<length()-1; i++){
-        item[i] = item[i+1];
-    }
-    itemNumber--;
-}
-
-template <class T>
-void List<T>::delNodeAtN(int n){
-    for(int i=n; i<length()-1; i++){
-        item[i] = item[i+1];
-    }
-    itemNumber--;
-}
-
-template <class T>
-bool List<T>::isEmpty(){
-    return itemNumber == -1;
-}
-
-template <class T>
-int List<T>::length() const{
-    return itemNumber+1;
-}
-
-template <class T>
-int List<T>::find(T it){
-    for(int i=0; i<length(); i++){
-        if(item[i]==it){
-            return i;
-        }
-    }
-    return -1;
-}
 
 #endif // LIST_HPP_INCLUDED
--- ListStack.hpp
+++ ListStack.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include "LinkedList.hpp"
+#include "LInkedList.cpp"
 #include "Constants.hpp"
 /*
    COPYRIGHT (C) 2017 Xiaofeng Qu (xq6 ) All rights reserved.
@@ -26,61 +27,4 @@ public:
     void divideTopTwo();
     T topItem();
 };
-
-template <class T>
-bool ListStack<T>::isEmpty(){
-    return llist.isEmpty();
-}
-
-template <class T>
-void ListStack<T>::push(T d){
-    llist.insertFront(d);
-}
-
-template <class T>
-void ListStack<T>::pop(){
-    llist.delFrontNode();
-}
-
-template <class T>
-T ListStack<T>::topItem(){
-    return llist.frontNode();
-}
-
-template <class T>
-void ListStack<T>::plusTopTwo(){
-    int top1 = topItem();
-    pop();
-    int top2 = topItem();
-    pop();
-    push(top2 + top1);
-}
-
-template <class T>
-void ListStack<T>::minusTopTwo(){
-    int top1 = topItem();
-    pop();
-    int top2 = topItem();
-    pop();
-    push(top2 - top1);
-}
-
-template <class T>
-void ListStack<T>::multiplyTopTwo(){
-    int top1 = topItem();
-    pop();
-    int top2 = topItem();
-    pop();
-    push(top2 * top1);
-}
-
-template <class T>
-void ListStack<T>::divideTopTwo(){
-    int top1 = topItem();
-    pop();
-    int top2 = topItem();
-    pop();
-    push(top2 / top1);
-}
-
 #endif // LISTSTACK_HPP_INCLUDED
